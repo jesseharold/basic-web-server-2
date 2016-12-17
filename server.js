3247c7168cb5016130fd47fd5b297446cdb27fcc
jesseharold <jesseharold@gmail.com>
2016-12-17 12:00:16 -0800

    added server that responds w html

--- server.js
+++ server.js
@@ -3,6 +3,16 @@
 var http = require('http');
 // built in, no need to install
 
+var url = require("url");
+
+
+function getPhrase(array){
+    var phraseIndex = 0;
+    phraseIndex = Math.floor(Math.random() * array.length);
+    return array[phraseIndex];
+}
+
+// ********************** server 1 *****************************
 
 var goodPhrases = [
     "You are a better dancer than I would have guessed to look at you.",
@@ -14,7 +24,6 @@ var goodPhrases = [
 ];
 
 var PORT = 7000;
-
 var server = http.createServer(handleRequest);
 server.listen(PORT, function(){
     console.log(`server is listening on http://localhost:${PORT}`);
@@ -24,6 +33,8 @@ function handleRequest(request, response){
     // a generic function to handle requests and responses
     response.end(getPhrase(goodPhrases));
 }
+
+// ********************** server 2 *****************************
 
 var badPhrases = [
     "You give up too easily",
@@ -36,7 +47,6 @@ var badPhrases = [
 ];
 
 var PORT2 = 7500;
-
 var server2 = http.createServer(handleRequest2);
 server2.listen(PORT2, function(){
     console.log(`server is listening on http://localhost:${PORT2}`);
@@ -47,8 +57,36 @@ function handleRequest2(request, response){
     response.end(getPhrase(badPhrases));
 }
 
-function getPhrase(array){
-    var phraseIndex = 0;
-    phraseIndex = Math.floor(Math.random() * array.length);
-    return array[phraseIndex];
+// ********************** server 3 *****************************
+
+var PORT3 = 8085;
+var server3 = http.createServer(handleRequest3);
+server3.listen(PORT3, function(){
+    console.log(`server is listening on http://localhost:${PORT3}`);
+});
+function handleRequest3(request, response){
+    var urlParts = url.parse(request.url);
+    // console.log(urlParts);
+    switch (urlParts.pathname) {
+        case "/":
+            displayRoot(urlParts.pathname, request, response);
+            break;
+        case "/portfolio":
+            displayRoot(urlParts.pathname, request, response);
+            break;
+        case "/edit":
+            console.log("display for editing a page");
+            break;
+        default:
+            console.log("404 not found");
+    }
+}
+
+function displayRoot(url, request, response){
+    var myHTML = `<html>
+        <body><h1>Home Page</h1>
+        <a href = "/portfolio">Portfolio</a>
+        </body></html>`;
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end(myHTML);
 }
